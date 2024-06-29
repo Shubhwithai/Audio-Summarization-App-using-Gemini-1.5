ad1d5bf94eaeb1687f2a5ea3d051531bddd903e7
Shubhwithai <shubham@buildfastwithai.com>
2024-06-29 14:03:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 import streamlit as st
-from pydub import AudioSegment
 import tempfile
 import os
-import google.generativeai as genai
-
+from google.generativeai import configure, GenerativeModel
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 # Configure Google API for audio summarization
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=GOOGLE_API_KEY)
+configure(api_key=GOOGLE_API_KEY)
 
 def summarize_audio(audio_file_path):
-    """Summarize the audio using Google's Generative API."""
-    model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
-    audio_file = genai.upload_file(path=audio_file_path)
-    response = model.generate_content(
-        [
+    """Summarize the audio using Google's Generative AI."""
+    model = GenerativeModel("gemini-1.5-pro-latest")
+    with open(audio_file_path, "rb") as audio_file:
+        response = model.generate_content([
             "Please summarize the following audio.",
-            audio_file
-        ]
-    )
+            {"mime_type": "audio/wav", "data": audio_file.read()}
+        ])
     return response.text
 
 def save_uploaded_file(uploaded_file):
@@ -39,16 +36,23 @@ st.title('Audio Summarization App')
 
 with st.expander("About this app"):
     st.write("""
-        This app uses Google's generative AI to summarize audio files. 
+        This app uses Google's Generative AI to summarize audio files. 
         Upload your audio file in WAV or MP3 format and get a concise summary of its content.
     """)
 
 audio_file = st.file_uploader("Upload Audio File", type=['wav', 'mp3'])
-if audio_file is not None:
-    audio_path = save_uploaded_file(audio_file)  # Save the uploaded file and get the path
-    st.audio(audio_path)
 
-    if st.button('Summarize Audio'):
-        with st.spinner('Summarizing...'):
-            summary_text = summarize_audio(audio_path)
-            st.info(summary_text)
+if audio_file is not None:
+    audio_path = save_uploaded_file(audio_file)
+    if audio_path:
+        st.audio(audio_file)
+        if st.button('Summarize Audio'):
+            with st.spinner('Summarizing...'):
+                try:
+                    summary_text = summarize_audio(audio_path)
+                    st.success("Summary generated successfully!")
+                    st.info(summary_text)
+                except Exception as e:
+                    st.error(f"An error occurred during summarization: {str(e)}")
+    else:
+        st.error("Failed to process the uploaded file.")
